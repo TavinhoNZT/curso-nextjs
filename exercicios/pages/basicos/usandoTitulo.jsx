9e8f33389bb76f80a0bd3ecc8452ae10d8f1ec83
Tavinho NZT <investindosabedoria@gmail.com>
2022-01-05 06:25:34 -0300

    confirmar em 'main'

---
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,12 @@
+import Titulo from '../../components/Titulo'
+
+export default function usandoTitulo() {
+    return (
+        <div>
+            <Titulo
+                principal="Página de Cadastro"
+                secundario="Incluir, alterar e excluir coisas!"
+            />
+        </div>
+    )
+}
