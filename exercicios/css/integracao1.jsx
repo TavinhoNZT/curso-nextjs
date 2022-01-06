3f1620ad6ad5aafb630141cc1f1108f8eb4a5023
Tavinho NZT <investindosabedoria@gmail.com>
2022-01-06 20:13:33 -0300

    css

---
+++ exercicios/css/integracao1.jsx
@@ -0,0 +1,11 @@
+import "./integracao1.css"
+
+export default function integracao() {
+    return (
+        <>
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branca">Texto #03</div>
+        </> 
+  )
+}
