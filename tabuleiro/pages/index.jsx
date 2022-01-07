5b48b949082d000f51f71d3800fe6fca322a896b
Tavinho NZT <investindosabedoria@gmail.com>
2022-01-07 06:56:41 -0300

    final

---
+++ tabuleiro/pages/index.jsx
@@ -0,0 +1,18 @@
+import Linha from '../components/Linha'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Linha />
+      <Linha preta />
+      <Linha />
+      <Linha preta />
+      <Linha />
+      <Linha preta />
+      <Linha />
+      <Linha preta />
+
+    </div>
+  )
+}
