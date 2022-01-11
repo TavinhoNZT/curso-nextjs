92da620803514e2c9df57e01660e33646cb8da5a
Tavinho NZT <investindosabedoria@gmail.com>
2022-01-11 06:29:43 -0300

    contador

---
+++ exercicios/components/ContadorDisplay.jsx
@@ -0,0 +1,20 @@
+export default function ContadorDisplay(props) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50px",
+        weight: "50px",
+        borderRadius: "25px",
+        backgroundColor: "#222",
+        color: "#fff",
+        fontSize: "2rem",
+        margin: "20px",
+      }}
+    >
+      {props.numero}
+    </div>
+  );
+}
